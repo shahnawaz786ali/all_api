51ad999fd1379c28fb52ed1d0bc8e465821916d7
shahnawaz786ali <shl.sagar12345@gmail.com>
2023-12-15 11:22:30 +0530

    commit all

--- DRF Study Material/Django REST Code/gs5/api/models.py
+++ DRF Study Material/Django REST Code/gs5/api/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Student(models.Model):
  name= models.CharField(max_length=100)
  roll = models.IntegerField()
- city= models.CharField(max_length=100)
+ city= models.CharField(max_length=200)
